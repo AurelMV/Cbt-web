0a00a5c0e3fc9435986eedd9d3314fc9c974efee
Botsito <ealanocca.231ds01@istta.edu.pe>
2024-11-01 15:16:40 -0500

    ApiDepa-Prov-Dist

--- app/Http/Controllers/Api/DepartamentoController.php
+++ app/Http/Controllers/Api/DepartamentoController.php
@@ -3,48 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Departamento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class DepartamentoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $departamento=Departamento::all();
+        return response()->json([
+            'status'=>true,
+            'message'=>'listado de datos',
+            'data'=>$departamento,
+        ],200
+        );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $departamento=Departamento::findOrFail($id);
+        return response()->json([
+            'status'=>true,
+            'message'=>'listado de datos',
+            'data'=>$departamento,
+        ],200
+        );
     }
 }
--- app/Http/Controllers/Api/DistritoController.php
+++ app/Http/Controllers/Api/DistritoController.php
@@ -3,49 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Distrito;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 
 class DistritoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $distrito=Distrito::all();
+        return response()->json([
+            'status'=>true,
+            'message'=>'listado de datos',
+            'data'=>$distrito,
+        ],200
+        );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $distrito=Distrito::findOrFail($id);
+        return response()->json([
+            'status'=>true,
+            'message'=>'listado de datos',
+            'data'=>$distrito,
+        ],200
+        );
     }
 }
