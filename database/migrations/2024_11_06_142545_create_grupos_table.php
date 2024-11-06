ce7b80ab2143527871d988b7948eed1c0a225c5d
Marco Valdez <marcotuamortlv@gmail.com>
2024-11-06 09:32:13 -0500

    Migraciones que me faltaban

---
+++ database/migrations/2024_11_06_142545_create_grupos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grupos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->integer('aforo');
+            $table->boolean('estado');
+            $table->foreignId('idciclo')->constrained('ciclos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grupos');
+    }
+};
