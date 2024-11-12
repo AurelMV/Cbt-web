59068a5985e02e609e7ddd009da73ed81b087b63
Botsito <ealanocca.231ds01@istta.edu.pe>
2024-11-11 22:39:13 -0500

    Actualización Api Colegios

--- app/Http/Controllers/Api/ProvinciaController.php
+++ app/Http/Controllers/Api/ProvinciaController.php
@@ -11,23 +11,46 @@ class ProvinciaController extends Controller
 {
     public function index()
     {
-        $provincia=Provincia::all();
-        return response()->json([
-            'status'=>true,
-            'message'=>'listado de datos',
-            'data'=>$provincia,
-        ],200
+        $provincia = Provincia::all();
+        return response()->json(
+            [
+                'status' => true,
+                'message' => 'listado de datos',
+                'data' => $provincia,
+            ],
+            200
         );
     }
 
     public function show(string $id)
     {
-        $provincia=Provincia::findOrFail($id);
-        return response()->json([
-            'status'=>true,
-            'message'=>'listado de datos',
-            'data'=>$provincia,
-        ],200
+        $provincia = Provincia::findOrFail($id);
+        return response()->json(
+            [
+                'status' => true,
+                'message' => 'listado de datos',
+                'data' => $provincia,
+            ],
+            200
         );
+    }
+    public function Consulta(string $id)
+    {
+        $provincias = Provincia::where('Departamento_idDepartamento', $id)->get();
+
+        // Verificar si se encontraron resultados
+        if ($provincias->isEmpty()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'No se encontraron provincias para este departamento',
+                'data' => [],
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Listado de provincias',
+            'data' => $provincias,
+        ], 200);
     }
 }
